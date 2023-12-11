681bd6f43e8bd595f0fbd8f331678025e12f3d3a
Galia <glyhdwnh556@gmail.com>
2023-12-11 15:55:39 +0200

    Adding the continuation of the home screen and posts.

---
+++ js/home_page.js
@@ -0,0 +1,9 @@
+function postOne(){
+    window.location.assign("../html/post_one.html");
+}
+function postTwo(){
+    window.location.assign("../html/post2.html");
+}
+function postThree(){
+    window.location.assign("../html/post3.html");
+}
