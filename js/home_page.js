d4b72e293d5164cbc5f3c66a21209587c00262e6
Maya Lev <maya.lev.2009@gmail.com>
2023-12-11 16:41:27 +0200

    renamed html files, deleted js files, renamed css file

--- js/home_page.js
+++
@@ -1,9 +0,0 @@
-function postOne(){
-    window.location.assign("../html/post_one.html");
-}
-function postTwo(){
-    window.location.assign("../html/post2.html");
-}
-function postThree(){
-    window.location.assign("../html/post3.html");
-}
